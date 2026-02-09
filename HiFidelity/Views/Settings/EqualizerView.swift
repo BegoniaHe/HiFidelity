9364b2e2382087a4386647f9f7fd5d05aa6a9404
BegoniaHe <tianyihebegonia1005@gmail.com>
2026-02-10 00:44:46 +0100

    fix(Equalizer): adjust EQ presets for improved audio balance

--- HiFidelity/Views/Settings/EqualizerView.swift
+++ HiFidelity/Views/Settings/EqualizerView.swift
@@ -536,7 +536,7 @@ enum EQPreset: String, CaseIterable, Identifiable {
             return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
         case .rock:
-            return [5, 3, -1, -2, 0, 2, 4, 5, 5, 4]
+            return [5, 3, -1, -2, 0, 2, 4, 5, 3, 2]
 
         case .pop:
             return [-2, -1, 0, 2, 4, 4, 2, 0, -1, -2]
@@ -551,13 +551,13 @@ enum EQPreset: String, CaseIterable, Identifiable {
             return [5, 4, 0, -2, 2, 4, 5, 6, 6, 5]
 
         case .bassBoost:
-            return [8, 7, 5, 3, 1, 0, 0, 0, 0, 0]
+            return [6, 5, 4, 2, 1, 0, 0, 0, 0, 0]
 
         case .trebleBoost:
-            return [0, 0, 0, 0, 0, 1, 3, 5, 7, 8]
+            return [0, 0, 0, 0, 0, 1, 2, 3, 4, 5]
 
         case .vocal:
-            return [-3, -2, 0, 3, 5, 5, 3, 0, -2, -3]
+            return [-3, -2, 0, 3, 5, 5, 3, 1, -1, -3]
 
         case .acoustic:
             return [4, 4, 3, 1, 2, 2, 3, 4, 4, 3]
